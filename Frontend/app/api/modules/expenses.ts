e73fc5b92b65628962d451ec198e3a147f5e7edd
Notley1608 <oscarnotley@hotmail.co.uk>
2026-08-06 18:07:50 +1000

    Updated store

--- Frontend/app/api/modules/expenses.ts
+++ Frontend/app/api/modules/expenses.ts
@@ -1,54 +1,61 @@
 import type { Expense, ExpensePayload } from "~/types/expenses";
-import { apiClient } from "../client";
+import { createApiClient } from "~/api/client";
 
-export const expensesApi = {
-  /**
-   * METHODS:
-   * createExpense
-   * getAllExpenses
-   * getMonthlySheet
-   * getMonthlySummary
-   * getExpense
-   * updateExpense
-   * deleteExpense
-   */
-  createExpense(payload: ExpensePayload): Promise<Expense> {
-    return apiClient<Expense>("/expenses", {
-      method: "POST",
-      body: payload,
-    });
-  },
-  getAllExpenses(): Promise<Expense[]> {
-    return apiClient<Expense[]>("/expenses", {
-      method: "GET",
-    });
-  },
-  getMonthlySheet(year: number, month: number): Promise<Expense[]> {
-    return apiClient<Expense[]>("/expenses/monthly-sheet", {
-      method: "GET",
-      query: { year, month },
-    });
-  },
-  getMonthlySummary(year: number, month: number): Promise<Expense[]> {
-    return apiClient<Expense[]>("/expenses/monthly-summary", {
-      method: "GET",
-      query: { year, month },
-    });
-  },
-  getExpense(expenseId: string): Promise<Expense> {
-    return apiClient<Expense>(`/expenses/${expenseId}`, {
-      method: "GET",
-    });
-  },
-  updateExpense(expenseId: string, payload: ExpensePayload): Promise<Expense> {
-    return apiClient<Expense>(`/expenses/${expenseId}`, {
-      method: "PATCH",
-      body: payload,
-    });
-  },
-  deleteExpense(expenseId: string): Promise<Expense> {
-    return apiClient<Expense>(`/expenses/${expenseId}`, {
-      method: "DELETE",
-    });
-  },
-};
+export function expensesApi() {
+  const apiClient = createApiClient();
+
+  return {
+    /**
+     * METHfODS:
+     * createExpense
+     * getAllExpenses
+     * getMonthlySheet
+     * getMonthlySummary
+     * getExpense
+     * updateExpense
+     * deleteExpense
+     */
+    createExpense(payload: ExpensePayload): Promise<Expense> {
+      return apiClient<Expense>("/expenses", {
+        method: "POST",
+        body: payload,
+      });
+    },
+    getAllExpenses(): Promise<Expense[]> {
+      return apiClient<Expense[]>("/expenses", {
+        method: "GET",
+      });
+    },
+    getMonthlySheet(year: number, month: number): Promise<Expense[]> {
+      return apiClient<Expense[]>("/expenses/monthly-sheet", {
+        method: "GET",
+        query: { year, month },
+      });
+    },
+    getMonthlySummary(year: number, month: number): Promise<Expense[]> {
+      return apiClient<Expense[]>("/expenses/monthly-summary", {
+        method: "GET",
+        query: { year, month },
+      });
+    },
+    getExpense(expenseId: string): Promise<Expense> {
+      return apiClient<Expense>(`/expenses/${expenseId}`, {
+        method: "GET",
+      });
+    },
+    updateExpense(
+      expenseId: string,
+      payload: ExpensePayload,
+    ): Promise<Expense> {
+      return apiClient<Expense>(`/expenses/${expenseId}`, {
+        method: "PATCH",
+        body: payload,
+      });
+    },
+    deleteExpense(expenseId: string): Promise<Expense> {
+      return apiClient<Expense>(`/expenses/${expenseId}`, {
+        method: "DELETE",
+      });
+    },
+  };
+}
