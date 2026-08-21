2c53d66c6f3243fe5313b25028074d00fcf507be
Notley1608 <oscarnotley@hotmail.co.uk>
2026-08-21 19:25:01 +1000

    dependency changes to runtime

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -2,5 +2,5 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 import * as schema from "./schemas/schema";
 
-const sqlite = new Database("database.sqlite.db");
+const sqlite = new Database(`${import.meta.dir}/database.sqlite.db`);
 export const db = drizzle(sqlite, { schema });
