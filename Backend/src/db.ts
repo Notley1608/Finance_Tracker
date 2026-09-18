4c851ccddb9de92ba32e09e70eea8990d2cc61c5
Notley1608 <oscarnotley@hotmail.co.uk>
2026-09-18 15:41:23 +1000

    containerised services

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -2,6 +2,7 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import * as schema from "./schemas/schema";
 
-const sqlite = new Database(`${import.meta.dir}/../database.sqlite.db`);
+const databasePath = Bun.env.DATABASE_PATH ?? `${import.meta.dir}/../database.sqlite.db`;
+const sqlite = new Database(databasePath);
 sqlite.run("PRAGMA foreign_keys = ON");
 export const db = drizzle(sqlite, { schema });
